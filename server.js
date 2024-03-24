179bd074d85de432b85bd4a5ab7460639a863b29
Rudra032 <rudrarathore2842@gmail.com>
2024-03-24 16:15:23 +0530

    server.js change

--- server.js
+++ server.js
@@ -3,15 +3,20 @@ import { db } from "./db.js";
 import roomsRoute from "./routes/roomsRouter.js";
 import userRouter from "./routes/userRouter.js";
 import bookingsRouter from './routes/bookingsRouter.js';
+import { fileURLToPath } from 'url';
 import path from "path";
+
 
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 5000;
-app.get('/', (req,res)=>{
-  app.use(express.static(path.resolve(__dirname, "my_rooms", "build")));
-  res.sendFile(path.resolve(__dirname, "my_rooms", "build", "index.html"));
+
+app.use(express.static(path.join(__dirname, "./my_rooms/build")));
+app.get('*', (req,res)=>{
+  res.sendFile(path.join(__dirname, './my_rooms/build/index.html'));
 })
+
+
 app.use("/api/mern_rooms", roomsRoute);
 app.use("/api/users", userRouter);
 app.use("/api/bookings", bookingsRouter);
